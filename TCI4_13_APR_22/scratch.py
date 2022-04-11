e99e64460ddd5f9ffffc259d4528e11615b8c8c9
Kyle Massey <kyle.massey@gmail.com>
2022-04-10 21:46:15 -0400

    tsc readme

--- TCI4_13_APR_22/scratch.py
+++ TCI4_13_APR_22/scratch.py
@@ -4,12 +4,11 @@ from pat import PAT_SECRET
 SERVER_URL = 'https://10ax.online.tableau.com/'
 SITE_NAME = 'kjmdev797388'
 
-tab_auth = TSC.PersonalAccessTokenAuth('Kyle', PAT_SECRET, site_id=SITE_NAME)
-server = TSC.Server(SERVER_URL, use_server_version=True)
+TAB_AUTH = TSC.PersonalAccessTokenAuth('Kyle', PAT_SECRET, site_id=SITE_NAME)
+SERVER = TSC.Server(SERVER_URL, use_server_version=True)
 
-with server.auth.sign_in(tab_auth):
-    new_ds = TSC.DatasourceItem(
-        'f2520dd7-858d-4271-915a-375ee01b8794', name='Extract Datasource 2')
 
-    server.datasources.publish(
-        new_ds, r"C:\Users\kylem\Downloads\Extract datasource.tdsx", mode='Overwrite')
+with SERVER.auth.sign_in(TAB_AUTH):
+    workbooks, paginator = SERVER.workbooks.get()
+
+    print([wb.name for wb in workbooks])
