631dd0505db19c7d15638a775937734e00e05b88
Kyle Massey <kyle.massey@gmail.com>
2022-04-12 22:58:07 -0400

    Add comments

--- TCI4_13_APR_22/a_introducing_tsc/signin_and_get_tsc.py
+++ TCI4_13_APR_22/a_introducing_tsc/signin_and_get_tsc.py
@@ -8,10 +8,12 @@ PAT_NAME = 'Kyle'
 
 
 def get_workbooks():
+    # There is no request body to sign in, it's handled here...
     tab_auth = TSC.PersonalAccessTokenAuth(
         PAT_NAME, PAT_SECRET, site_id=SITE_NAME)
     server = TSC.Server(SERVER_URL, use_server_version=True)
 
+    #... and here
     with server.auth.sign_in(tab_auth):
         workbooks, paginator = server.workbooks.get()
 
