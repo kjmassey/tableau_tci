2f2459c5018a6c8d01bb3829802456e3d9954ee3
Kyle Massey <kyle.massey@gmail.com>
2022-04-12 22:17:55 -0400

    Finish build a project

--- TCI4_13_APR_22/e_build_a_project/build_a_project.py
+++ TCI4_13_APR_22/e_build_a_project/build_a_project.py
@@ -1,3 +1,4 @@
+from audioop import add
 from math import trunc
 import tableauserverclient as TSC
 from pat import PAT_SECRET
@@ -9,6 +10,10 @@ SERVER_URL = 'https://10ax.online.tableau.com'
 SITE_NAME = 'kjmdev797388'
 
 WB_PATH = "C:\\DEV\\Tableau\\tableau_tci\\TCI4_13_APR_22\\e_build_a_project\\Extract workbook.twbx"
+
+########
+# Let's put it all together and build a very basic project with groups, permissions, initial content and a refresh!
+########
 
 
 def get_tab_auth_server():
@@ -70,7 +75,9 @@ def assign_group_permissions(group_luid, project_name):
         return updated_project
 
 
-def add_user_to_group(group_name, user_email):
+def add_user_to_group(group_name):
+    user_email = input('Enter User\'s Email: ')
+
     tab_auth, server = get_tab_auth_server()
 
     with server.auth.sign_in(tab_auth):
@@ -86,4 +93,49 @@ def add_user_to_group(group_name, user_email):
         return updated_group.__dict__
 
 
-def
+def publish_initial_workbook(proj_luid, wb_path):
+    wb_filename = wb_path.split('\\')[-1]
+    wb_name = wb_filename.split('.')[0]
+
+    tab_auth, server = get_tab_auth_server()
+
+    with server.auth.sign_in(tab_auth):
+        new_wb = TSC.WorkbookItem(project_id=proj_luid, name=wb_name)
+
+        creds = TSC.ConnectionCredentials(db_creds.USER, db_creds.PW)
+        creds.embed = True
+
+        published_wb = server.workbooks.publish(
+            new_wb, wb_path, connection_credentials=creds, mode='Overwrite')
+
+        return published_wb.__dict__
+
+
+def refresh_workbook(wb_luid):
+    tab_auth, server = get_tab_auth_server()
+
+    with server.auth.sign_in(tab_auth):
+        return server.workbooks.refresh(wb_luid).__dict__
+
+
+def build_a_project_from_scratch():
+    project = create_new_project()
+    print(f"- Created project: {project['_name']}")
+
+    group = create_server_group()
+    print(f"- Created group: {group['_name']}")
+
+    permissions = assign_group_permissions(group['_id'], project['_name'])
+    print('- Applied permissions')
+
+    users_to_group = add_user_to_group(group['_name'])
+    print('- Added user to group')
+
+    workbook = publish_initial_workbook(project['_id'], WB_PATH)
+    print('- Published initial workbook')
+
+    refresh = refresh_workbook(workbook['_id'])
+    print('- Initiated extract refresh, job id: ', refresh['_id'])
+
+
+build_a_project_from_scratch()
